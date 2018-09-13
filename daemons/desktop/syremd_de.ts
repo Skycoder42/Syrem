f48768d694034ac749af176e35e4e900d09b4226
Skycoder42 <Skycoder42@users.noreply.github.com>
2018-09-13 10:45:21 +0200

    fix flatpak translations

--- daemons/desktop/syremd_de.ts
+++ daemons/desktop/syremd_de.ts
@@ -6,6 +6,7 @@
 		<dependency catalog="qtwebsockets_de"/>
 		<dependency catalog="qtdatasync_de"/>
 		<dependency catalog="qtmvvmcore_de"/>
+		<dependency catalog="qtmvvmwidgets_de"/>
 
 		<dependency catalog="syrem_lib_de"/>
 		<dependency catalog="syrem_daemon_de"/>
--- gui/core/syrem_core_de.ts
+++ gui/core/syrem_core_de.ts
@@ -386,7 +386,7 @@ remind-me --add [--important] &lt;Beschreibung&gt; &lt;Wann&gt;</translation>
 <context>
     <name>settings.xml</name>
     <message>
-        <location filename="../../../build-Syrem-Desktop_Qt_5_11_1_GCC_64bit-Debug/gui/core/.tr-dummies/settings.tsdummy.cpp" line="+7"/>
+        <location filename="../../../build-Syrem-Android_f_r_x86_GCC_4_9_Qt_5_11_1_for_Android_x86-Debug/gui/core/.tr-dummies/settings.tsdummy.cpp" line="+7"/>
         <source>General</source>
         <translation>Allgemein</translation>
     </message>
